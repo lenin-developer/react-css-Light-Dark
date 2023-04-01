1c4aae027499d93d5cfc823e29dc35e89e7cdb6e
lenin cruz mayo <40728531+lenin-developer@users.noreply.github.com>
2023-03-31 18:08:29 -0600

    build: add postcss-preset-env

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import postcssPresetEnv from "postcss-preset-env";
 
 // https://vitejs.dev/config/
 export default ({ mode }) => {
@@ -9,6 +10,26 @@ export default ({ mode }) => {
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    css: {
+      postcss: {
+        plugins: [
+          postcssPresetEnv({
+            stage: false,
+            features: {
+              "nesting-rules": true,
+              "custom-media-queries": true,
+              /**
+               * "blank-pseudo-class": false,
+               * "focus-visible-pseudo-class": false,
+               * "focus-within-pseudo-class": false,
+               * "has-pseudo-class": false,
+               * "prefers-color-scheme-query": false
+               */
+            },
+          }),
+        ],
       },
     },
     define: { "process.env": { ...loadEnv(mode, process.cwd()) } },
