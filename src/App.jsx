a86d124b9404716ac4e03a01231ee9ec3f197460
lenin cruz mayo <40728531+lenin-developer@users.noreply.github.com>
2023-04-03 16:42:41 -0600

    feat: maquetado principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Header } from '@/components'
+import styles from './App.module.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0)
+	console.log('desde app')
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+	return (
+		<div className={styles.app}>
+			<Header>
+				<p>hola</p>
+			</Header>
+		</div>
+	)
 }
 
 export default App
