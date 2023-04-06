c9f95ce642b5535a30f76723597056b854ba4f12
lenin cruz mayo <40728531+lenin-developer@users.noreply.github.com>
2023-04-05 20:36:13 -0600

    feat: comp: ItemTheme #new

---
+++ src/components/atomic/ItemTheme/ItemTheme.jsx
@@ -0,0 +1,16 @@
+import { node, string } from 'prop-types'
+import styles from './ItemTheme.module.css'
+
+export const ItemTheme = ({ label, children, ...res }) => {
+	return (
+		<li className={styles.item} id={label} role='opcion' tabindex={-1} aria-selected={'false'} {...res}>
+			<figure className={styles.item__icon}>{children}</figure>
+			<p className={styles.item__text}>{label}</p>
+		</li>
+	)
+}
+
+ItemTheme.propTypes = {
+	label: string,
+	children: node,
+}
