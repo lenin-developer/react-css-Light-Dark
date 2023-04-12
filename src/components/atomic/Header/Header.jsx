af1734cc4c1a7c42a46b54b306d34ec5110e5436
lenin cruz mayo <40728531+lenin-developer@users.noreply.github.com>
2023-04-12 15:28:02 -0600

    style: Comp: Header #modify

--- src/components/atomic/Header/Header.jsx
+++ src/components/atomic/Header/Header.jsx
@@ -4,7 +4,7 @@ import styles from './Header.module.css'
 export const Header = ({ label, children }) => {
 	return (
 		<header className={styles.header}>
-			<h2>{label}</h2>
+			<h2 className={styles.header__tilte}>{label}</h2>
 			<div className={styles.header__itemSection}>{children}</div>
 		</header>
 	)
